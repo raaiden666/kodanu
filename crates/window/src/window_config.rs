6d52784ac8251854522aee5946505822ea9ee603
raaiden666 <raaiden666@gmail.com>
2026-06-05 23:00:02 +0500

    feat(window): add fullscreen toggle

--- crates/window/src/window_config.rs
+++ crates/window/src/window_config.rs
@@ -8,16 +8,16 @@ pub struct WindowConfig {
     height: u32,
     min_width: u32,
     min_height: u32,
+    maximized: bool,
 }
 
 impl WindowConfig {
     pub const DEFAULT_TITLE_STR: &str = "REngine";
-
     pub const DEFAULT_WIDTH: u32 = 1280;
     pub const DEFAULT_HEIGHT: u32 = 720;
-
-    pub const DEFAULT_MIN_WIDTH: u32 = 800;
-    pub const DEFAULT_MIN_HEIGHT: u32 = 600;
+    pub const DEFAULT_MIN_WIDTH: u32 = 600;
+    pub const DEFAULT_MIN_HEIGHT: u32 = 400;
+    pub const DEFAULT_MAXIMIZED: bool = true;
 }
 
 impl Default for WindowConfig {
@@ -28,6 +28,7 @@ impl Default for WindowConfig {
             height: Self::DEFAULT_HEIGHT,
             min_width: Self::DEFAULT_MIN_WIDTH,
             min_height: Self::DEFAULT_MIN_HEIGHT,
+            maximized: Self::DEFAULT_MAXIMIZED,
         }
     }
 }
@@ -38,8 +39,11 @@ impl WindowConfig {
             .with_title(&self.title)
             .with_inner_size(Size::clamped(self.width, self.height))
             .with_min_inner_size(Size::clamped(self.min_width, self.min_height))
+            .with_maximized(self.maximized)
     }
+}
 
+impl WindowConfig {
     pub fn with_title(mut self, title: impl Into<String>) -> Self {
         self.title = title.into();
         self
@@ -54,6 +58,11 @@ impl WindowConfig {
     pub fn with_min_size(mut self, min_width: u32, min_height: u32) -> Self {
         self.min_width = min_width.max(1);
         self.min_height = min_height.max(1);
+        self
+    }
+
+    pub fn with_maximized(mut self, maximized: bool) -> Self {
+        self.maximized = maximized;
         self
     }
 }
