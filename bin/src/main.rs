b9242be1f7cb7047e3937a352bb849f69e5fc3a6
raaiden666 <raaiden666@gmail.com>
2026-06-15 22:18:26 +0500

    fix(tracing): change create EnvFilter for calloop

--- bin/src/main.rs
+++ bin/src/main.rs
@@ -3,15 +3,11 @@ use {anyhow::Result, app::App};
 use tracing_subscriber::{EnvFilter, fmt};
 
 fn main() -> Result<()> {
-    let wgpu_hal_filter = EnvFilter::new("info").add_directive("wgpu_hal=error".parse().unwrap());
+    let wgpu_hal_filter = EnvFilter::new("info")
+        .add_directive("wgpu_hal=error".parse().unwrap())
+        .add_directive("calloop=off".parse().unwrap());
 
-    let callop_filter =
-        EnvFilter::new("warn").add_directive("calloop::loop_logic=error".parse().unwrap());
-
-    fmt()
-        .with_env_filter(wgpu_hal_filter)
-        .with_env_filter(callop_filter)
-        .init();
+    fmt().with_env_filter(wgpu_hal_filter).init();
 
     App::run()?;
 
